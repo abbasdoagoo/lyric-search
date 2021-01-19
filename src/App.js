c6e0d39298948d9b08fa16bc24309d8b7b6396f1
abbasdoagoo <a.doagou@gmail.com>
2021-01-20 01:37:06 +0330

    new feature

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/layout/Navbar";
 import Index from "./components/layout/Index";
 import "./App.css";
 import { Provider } from "./context";
+import Lyrics from "./components/tracks/Lyrics"
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <div className="container">
             <Switch>
               <Route exact path="/" component={Index} />
+              <Route exact path="/Lyrics/track/:id" component={Lyrics} />
             </Switch>
           </div>
         </React.Fragment>
