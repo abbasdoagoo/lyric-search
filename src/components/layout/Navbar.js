11bcbbcdd1c7ec9ef0a5a370aafa50bccb1211e6
abbasdoagoo <a.doagou@gmail.com>
2021-01-19 16:15:09 +0330

    commit all

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+ function Navbar() {
+    return (
+        <nav className="navbar navbar-dark mb-5 bg-dark">
+            <span className="navbar-brand h1 mb-0 mx-auto">LyricFinder</span>
+        </nav>
+    )
+}
+
+export default Navbar;
